8fa673275a2e225aba3a9c8c31319c1a31256cac
Norman Moeschter <norman.moeschter@gmail.com>
2020-07-23 16:45:07 +0100

    Make hostname dynamic.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,17 @@
-#!/bin/sh
+#!/bin/ash
+
+if [ -n "$HOSTNAME" -a -n "$PRIVATE_KEY" ]; then
+    echo "'Make service directory /var/lib/tor/$SERVICE_NAME"
+    mkdir -p /var/lib/tor/"$SERVICE_NAME"
+    chmod 700 /var/lib/tor/"$SERVICE_NAME"
+    echo "$HOSTNAME" > /var/lib/tor/"$SERVICE_NAME"/hostname
+    chmod 600 /var/lib/tor/"$SERVICE_NAME"/hostname
+    echo "$PRIVATE_KEY" > /var/lib/tor/"$SERVICE_NAME"/private_key
+    chmod 600 /var/lib/tor/"$SERVICE_NAME"/private_key
+    sed -i "s|\_\_servicename\_\_|$SERVICE_NAME|g" /etc/tor/torrc.template
+    sed -i "s|\_\_serviceport\_\_|$SERVICE_PORT|g" /etc/tor/torrc.template
+    cp /etc/tor/torrc.template /etc/tor/torrc
+fi
 
 chown -R tor /var/lib/tor
 
